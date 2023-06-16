1efcc096adf3504a4a04d377e35c081169faf7da
YTorresL <yalithtorres@gmail.com>
2023-06-16 00:56:23 -0400

    Close

--- components/Modal/page.js
+++ components/Modal/page.js
@@ -6,10 +6,7 @@ export default function Modal({ isOpen, onClose, children }) {
   return (
     <>
       {isOpen && (
-        <div
-          className="fixed top-0 bottom-0 right-0 left-0 bg-black/50 z-50"
-          onClick={handleClose}
-        >
+        <div className="fixed top-0 bottom-0 right-0 left-0 bg-black/50 z-50">
           <div className="bg-white rounded-lg w-[90%] h-[90%] mx-auto lg:mt-[2.5%] mt-[10%]">
             <div className="w-[95%] mx-auto">
               <button
